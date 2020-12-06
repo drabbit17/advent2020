49346c18dc2747c4f0a6779ccdfceb86400b4dec
drabbit17 <matteo.pallini17@gmail.com>
2020-12-06 10:28:32 +0000

    add solutions for first 2 problems

---
+++ solutions.py
@@ -0,0 +1,54 @@
+from typing import List
+import collections
+
+
+def load_txt_file(file_path: str) -> List[str]:
+    with open(file_path, 'r') as handle:
+        content = handle.read().splitlines()
+    return content
+
+
+def problem_1a(data: List[str], target = 2020) -> int:
+    data = [int(e) for e in data]
+    complementaries = {target-e: e for e in data}
+
+    for e in data:
+        if complementaries.get(e):
+            return e*complementaries.get(e)
+
+def problem_1b(data: List[str], target: int = 2020) -> int:
+    data = sorted([int(e) for e in data])
+
+    for idx, e in enumerate(data):
+        low, high = idx+1, len(data)-1
+        while low<high:
+            if data[low]+data[idx]+data[high]>target:
+                high -= 1
+            elif data[low]+data[idx]+data[high]<target:
+                low+=1
+            else:
+                return data[low] * data[idx] * data[high]
+
+def problem_2a(data: List[str]) -> int:
+    data_parsed = [[w.strip() for w in e.split(":")] for e in data]
+    matches = []
+    for e in data_parsed:
+        boundaries, letter = e[0].split(" ")
+        low, high = boundaries.split("-")
+        letters = collections.Counter(e[1])
+        if int(low)<=letters[letter]<=int(high):
+            matches.append(e)
+    return len(matches)
+
+
+def problem_2b(data: List[str]) -> int:
+    data_parsed = [[w.strip() for w in e.split(":")] for e in data]    
+    matches = []
+    for e in data_parsed:
+        boundaries, letter = e[0].split(" ")
+        low, high = boundaries.split("-")
+        low_l, high_l = e[1][int(low)-1], e[1][int(high)-1]
+        if (int(low_l==letter) + int(high_l==letter)) == 1:
+            matches.append(e)
+    return len(matches)
+
