b92c81d5ae6fd149843ef9fd5c3e6705dd294122
drabbit17 <matteo.pallini17@gmail.com>
2020-12-14 10:13:50 +0000

    add solution for 11

--- solutions.py
+++ solutions.py
@@ -1,5 +1,8 @@
 from typing import List, Dict
+import copy
 import collections
+import itertools
+import functools
 import math
 import re
 
@@ -355,6 +358,64 @@ def problem10b(data):
     return paths_count
 
 
+def problem11a(data):
+    grid_stable = False
+    while not grid_stable:
+        new_grid = change_signs_for_grid_problem11a(data, get_neighbors, 4)
+        grid_stable = new_grid == data
+        data = new_grid
+    return sum([seat=="#" for row in data for seat in row])
+
+def change_signs_for_grid_problem11a(data, get_neighbors_function, taken_seats_threshold):
+    left_bound, right_bound, lower_bound, upper_bound = 0, len(data[0])-1, 0, len(data)-1
+    taken_sign, no_seat_sign, free_sign = "#", ".", "L"
+    new_data = copy.deepcopy(data)
+    for row_idx, row in enumerate(data):
+        for e_idx, val in enumerate(row):
+            if val != no_seat_sign:
+                taken_seats = 0
+                for x, y in get_neighbors_function(e_idx, row_idx):
+                    if (left_bound <= x <= right_bound) and (lower_bound <= y <= upper_bound):
+                        taken_seats += data[y][x] == taken_sign
+
+                new_value = new_value_for_seat(taken_seats, data[row_idx][e_idx], free_sign, taken_sign, taken_seats_threshold)
+                if new_value:
+                    new_data[row_idx] = new_data[row_idx][:e_idx] + new_value + new_data[row_idx][e_idx+1:]
+    return new_data
 
 
+def new_value_for_seat(seats_taken, current_seat_value, free_sign, taken_sign, seats_taken_threshold):
+    if seats_taken == 0 and current_seat_value:
+        return taken_sign
+    elif seats_taken >= seats_taken_threshold:
+        return free_sign
+    else:
+        return None
+
+
+def get_neighbors(x_val, y_val):
+    possible_movements = list(itertools.permutations([1, -1, 0], 2)) + [(1, 1), (-1, -1)]
+    return [(x_val+x_shift, y_val+y_shift) for x_shift, y_shift in possible_movements]
+
+def get_neighbors_first_seat_seen(x_val, y_val, data):
+    possible_movements = list(itertools.permutations([1, -1, 0], 2)) + [(1, 1), (-1, -1)]
+    final_values = []
+    for x_shift, y_shift in possible_movements:
+        counter = 1
+        new_x, new_y = x_val + x_shift * counter, y_val + y_shift * counter
+        while (0 <= new_x < len(data[0])) and (0 <= new_y < len(data)) and (data[new_y][new_x] == "."):
+            counter += 1
+            new_x, new_y = x_val + x_shift * counter, y_val + y_shift * counter
+        final_values.append((new_x, new_y))
+    return final_values
+
+def problem11b(data):
+    grid_stable = False
+
+    while not grid_stable:
+        get_neighbors_function = functools.partial(get_neighbors_first_seat_seen, data=data)
+        new_grid = change_signs_for_grid_problem11a(data, get_neighbors_function, 5)
+        grid_stable = new_grid == data
+        data = new_grid
+    return sum([seat=="#" for row in data for seat in row])
 
