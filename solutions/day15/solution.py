6a6925e49feabab247a7fbbaafb85a0b877eb885
drabbit17 <matteo.pallini17@gmail.com>
2020-12-19 13:31:21 +0000

    add solution to problem 15

---
+++ solutions/day15/solution.py
@@ -0,0 +1,16 @@
+def problem15a(data, stop_point):
+    numbers = [int(e) for e in data.split(",")]
+    counter, last_time_memo = len(numbers) - 1, {
+        k: count for k, count in zip(numbers, range(1, len(numbers)))
+    }
+    next_number = numbers[-1]
+
+    while counter < stop_point - 1:
+        last_count = last_time_memo.get(next_number, 0)
+        counter += 1
+        last_time_memo[next_number] = counter
+        if last_count == 0:
+            next_number = 0
+        else:
+            next_number = counter - last_count
+    return next_number, last_time_memo
